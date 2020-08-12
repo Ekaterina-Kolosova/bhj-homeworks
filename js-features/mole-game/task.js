f42e98c22f8313640b6f8c53c191d4710ce045a5
Ekaterina-Kolosova <kolosova411@yandex.ru>
2020-08-12 22:26:43 +0300

    mole-game change

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,6 +1,6 @@
 
-const dead = document.getElementById("dead");
-const lost = document.getElementById("lost");
+let dead = document.getElementById("dead");
+let lost = document.getElementById("lost");
 
 for (let i = 1;  i < 10; i++) {
 	getHole = index => document.getElementById(`hole${index}`);
@@ -10,13 +10,19 @@ for (let i = 1;  i < 10; i++) {
 			} else {
 				lost.textContent = +lost.textContent + 1;
 			};
+			
+			if (dead.textContent === '10') {
+				alert('Вы выиграли');
+				resetResult();
+			} else if (lost.textContent === '5') {
+				alert('Вы проиграли');
+				resetResult();
+			};
 		};
+	
 };
 
-if (dead.textContent === 10) {
-	alert('Вы выиграли');
-	dead.textContent = 0;
+function resetResult () {
 	lost.textContent = 0;
-} else if (lost.textContent === 5) {
-	alert('Вы проиграли');
+	dead.textContent = 0;
 };
